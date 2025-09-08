dd8f5d483da8eaf6ac94b90a0cff4f48637129ec
aishavrs <aishaashiruu@gmail.com>
2025-09-08 12:56:40 +0100

    Replaced the endpoints with deloy link

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await fetch("http://localhost:5000/api/user/signin", {
+      const response = await fetch("https://desserts-factory-auth.onrender.com/api/user/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -44,7 +44,7 @@ export default function SignUpPage() {
     setLoading(true);
 
     try {
-      const response = await fetch(`http://localhost:5000/api/user/signup`, {
+      const response = await fetch(`https://desserts-factory-auth.onrender.com/api/user/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
